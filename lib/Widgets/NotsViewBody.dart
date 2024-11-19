15adfd48d0f022af84255845e0292f450260b5ce
amira <amira7assan4444@gmail.com>
2024-11-19 23:00:41 +0200

    notsView done

--- lib/Widgets/NotsViewBody.dart
+++ lib/Widgets/NotsViewBody.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:nots_app/Widgets/CustomAppPar.dart';
+import 'package:nots_app/Widgets/CustomNotsIteam.dart';
 class NotsViewBody extends StatelessWidget {
   const NotsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
           SizedBox(
             height: 50,
           ),
-          CustomAppPar()
+          CustomAppPar(),
+          NoteItem(),
         ],
       ),
     );
--- lib/views/nots_view.dart
+++ lib/views/nots_view.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/Widgets/CustomNotsIteam.dart';
 import 'package:nots_app/Widgets/NotsViewBody.dart';
 class NotsView extends StatelessWidget {
   const NotsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: NotsViewBody(),
+
     );
   }
 }
-
