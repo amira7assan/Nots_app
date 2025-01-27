9d1439f2ae080fd58089da6c5da271a9fb36d8d3
amira <amira7assan4444@gmail.com>
2025-01-27 17:46:36 +0200

    UI finish

--- lib/Widgets/EditeNoteViewBody.dart
+++ lib/Widgets/EditeNoteViewBody.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:nots_app/Widgets/CustomAppPar.dart';
+import 'package:nots_app/Widgets/CustomTextField.dart';
 class EditeNoteViewBody extends StatelessWidget {
   const EditeNoteViewBody({super.key});
 
@@ -14,7 +15,13 @@ class EditeNoteViewBody extends StatelessWidget {
         height: 50,
       ),
           CustomAppPar(title: 'Edite Note', icon: Icons.check,),
-        ],
+      Customtextfield(hint: 'Title',),
+          SizedBox(
+            height: 50,
+          ),
+      Customtextfield(hint: 'content', maxline: 5,),
+
+      ],
       ),
     );
   }
