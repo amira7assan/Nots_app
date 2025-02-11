cb0d3521b0c9fb2375b7251a3b16f8c5cbf68ffc
amira <amira7assan4444@gmail.com>
2025-02-11 16:20:59 +0200

    create type adabter for noteNodet(noteModel.g)

--- lib/models/noteModel.dart
+++ lib/models/noteModel.dart
@@ -1,8 +1,15 @@
 import "package:flutter/material.dart";
-class NoteModel{
+import 'package:hive/hive.dart';
+part 'noteModel.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
 NoteModel({
