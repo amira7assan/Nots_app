d479316e07e9c8d942daacbfabcc57d39314ddc1
amira <amira7assan4444@gmail.com>
2025-03-07 16:27:33 +0200

    create fetch note method

--- lib/cubits/notesCubit/notes_cubit.dart
+++ lib/cubits/notesCubit/notes_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:nots_app/Widgets/const.dart';
 import 'package:nots_app/models/noteModel.dart';
 
 part 'notes_state.dart';
 
 class NotsCubit extends Cubit<NotsState> {
   NotsCubit() : super(NotsInitial());
-}
+
+  fetchAllNotes()async{
+    try{
+      var notsBox = Hive.box<NoteModel>(KNotesBox);
+     List<NoteModel>notes= notsBox.values.toList();
+      emit(NotsSuccess(notes));
+    }
+    catch(e)
+    {
+      emit(NotsFailure(e.toString()));
+    }
+  }
+  }
+
--- lib/views/nots_view.dart
+++ lib/views/nots_view.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nots_app/Widgets/NotsViewBody.dart';
 import 'package:nots_app/Widgets/AddNoteButtomSheet.dart';
+import 'package:nots_app/cubits/AddNoteCubit/add_note_cubit.dart';
+
+import '../cubits/notesCubit/notes_cubit.dart';
+
 class NotsView extends StatelessWidget {
   const NotsView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
-              context: context, builder: (context){
-            return const AddNoteButtomSheet();
-          });
-        },
-        backgroundColor: Colors.blueGrey, 
-        child: Icon(
-          Icons.add,
-          color: Colors.white, // Icon color
+    return BlocProvider(
+      create: (context) => NotsCubit(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                ),
+                context: context, builder: (context) {
+              return const AddNoteButtomSheet();
+            });
+          },
+          backgroundColor: Colors.blueGrey,
+          child: Icon(
+            Icons.add,
+            color: Colors.white, // Icon color
+          ),
         ),
+        body: NotsViewBody(),
       ),
-      body: NotsViewBody(),
     );
   }
 }
